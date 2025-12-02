b0174a72055a34ffe19bf9675b382f80b365eb2b
m-inazawa <m.inazawa@infomart.co.jp>
2025-12-02 17:34:12 +0900

    バリデーションの設定

--- src/main/java/in/tech_camp/chat_app/repository/UserRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/UserRepository.java
@@ -23,4 +23,11 @@ public interface UserRepository {
 
   @Update("UPDATE users SET name = #{name}, email = #{email} WHERE id = #{id}")
   void update(UserEntity user);
+
+  // メールアドレスが使用済みであるかを確認するメソッド
+  @Select("SELECT EXISTS(SELECT 1 FROM users WHERE email = #{email})")
+  boolean existsByEmail(String email);
+
+  @Select("SELECT COUNT(*) > 0 FROM users WHERE email = #{email} AND id != #{userId}")
+  boolean existsByEmailExcludingCurrent(String email, Integer userId);
 }
