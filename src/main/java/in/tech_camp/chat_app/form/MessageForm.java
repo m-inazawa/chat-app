b8383f55e3944f17181d80a0869983371b7be5b9
m-inazawa <m.inazawa@infomart.co.jp>
2025-12-04 14:34:13 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- src/main/java/in/tech_camp/chat_app/form/MessageForm.java
+++ src/main/java/in/tech_camp/chat_app/form/MessageForm.java
@@ -1,15 +1,23 @@
 package in.tech_camp.chat_app.form;
 
+import org.springframework.validation.BindingResult;
 import org.springframework.web.multipart.MultipartFile;
 
-import in.tech_camp.chat_app.validation.ValidationPriority1;
-import jakarta.validation.constraints.NotBlank;
+// import in.tech_camp.chat_app.validation.ValidationPriority1;
+// import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class MessageForm {
-  @NotBlank(message = "Name can't be blank",groups = ValidationPriority1.class)
+  // @NotBlank(message = "Name can't be blank",groups = ValidationPriority1.class)
   private String content;
 
   private MultipartFile image;
+
+  public void ValidateMessage(BindingResult result) {
+    if ((content == null || content.isEmpty()) && (image == null || image.isEmpty())) {
+      result.rejectValue("content", "error.Message", "please enter either content or image");
+    }
+  }
+
 }
