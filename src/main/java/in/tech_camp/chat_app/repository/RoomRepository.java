67ec3d6addbf4f9914a8e21e6a1ca572db77f7a5
m-inazawa <m.inazawa@infomart.co.jp>
2025-12-04 11:50:50 +0900

    チャットルームの削除機能の実装

--- src/main/java/in/tech_camp/chat_app/repository/RoomRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/RoomRepository.java
@@ -1,5 +1,6 @@
 package in.tech_camp.chat_app.repository;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
@@ -13,6 +14,9 @@ public interface RoomRepository {
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void insert(RoomEntity roomEntity);
 
-  @Select("SELECT *FROM rooms WHERE id = #{id}")
+  @Select("SELECT * FROM rooms WHERE id = #{id}")
   RoomEntity findById(Integer id);
+
+  @Delete("DELETE FROM rooms WHERE id = #{id}")
+  void deleteById(Integer id);
 }
